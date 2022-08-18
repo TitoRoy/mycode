78f97f6cb2315c8cc54293a0d27ae2d0671f084f
Tito Roy <roytito@yahoo.com>
2022-08-18 20:02:09 +0000

    test

--- miniprojects/financial_project.py
+++ miniprojects/financial_project.py
@@ -3,23 +3,30 @@
 
 def financial_calculator():
     """ Calculate your Car or Mortgage payment"""
-    price = float(input(" What is the total price of your car or home? Please enter the numeric value:"))
-    downpayment = float(input( "How much money you would like to put as downpayment? Please enter the numeric value:"))
-    loan_amount = price - downpayment
+    try:
+        price = float(input(" What is the total price of your car or home? Please enter the numeric value:\n>"))
+        downpayment = float(input( "How much money you would like to put as downpayment? Please enter the numeric value:\n>"))
+        loan_amount = price - downpayment
 
-    loanterm = int(input("What is the length of the loan in years? Please enter the numeric value:"))
-    #Claculate the loan length in months
-    loanterm_month = loanterm * 12
-    
-    #print(monthly_payment)
-    interestrate = float(input("What is the yearly interest rest?\n>"))
-    monthly_rate = interestrate / 12 /100
-    #    print(monthly_rate)     
-    #Calculate the monthly payment
-    monthly_payment = (monthly_rate * loan_amount * pow((1 + monthly_rate), loanterm_month))  / (pow((1 + monthly_rate), loanterm_month) - 1)
-    monthly_payment = "{:.2f}".format(monthly_payment)
-#    print(f'Your monthly payment is: {monthly_payment}') 
-    return  f'Your monthly payment is: {monthly_payment}'
+        loanterm = int(input("What is the length of the loan in years? Please enter the numeric value:\n>"))
+        #Claculate the loan length in months
+        loanterm_month = loanterm * 12
+        
+        #print(monthly_payment)
+        interestrate = float(input("What is the yearly interest rest?\n>"))
+        monthly_rate = interestrate / 12 /100
+        #    print(monthly_rate)     
+        #Calculate the monthly payment
+        monthly_payment = (monthly_rate * loan_amount * pow((1 + monthly_rate), loanterm_month))  / (pow((1 + monthly_rate), loanterm_month) - 1)
+        monthly_payment = "{:.2f}".format(monthly_payment)
+    #    print(f'Your monthly payment is: {monthly_payment}') 
+        return  f'Your monthly payment is: {monthly_payment}'
+    #except OverflowError:
+        #return "The error occurred because xyz"
+    except ValueError:
+        return "That was not a number. Please put a number."
+    except:
+        return "There was an error!"
 
 def another_payment():
     wrong_input = 0    
@@ -40,6 +47,8 @@ def another_payment():
 
 def main():
     print(financial_calculator())
+    #print("Your monthly payment is...")
+    #print("There was an error!")
     another_payment()
 
 if __name__ == "__main__":
--- miniprojects/readdataproject.py
+++ miniprojects/readdataproject.py
@@ -4,7 +4,7 @@ import requests
 from pprint import pprint
 
 # 1. get an api key, and use a different company (MSFT)
-url = 'https://www.'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=15min&apikey=2SnXduIBPDy9UnlWKOrxkKL_U0ZfmK45'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=15min&apikey=2SnXduIBPDy9UnlWKOrxkKL_U0ZfmK45'
 r = requests.get(url)
 data = r.json()
 
@@ -14,16 +14,32 @@ high_list= []
 # looping over all time dictionaries
 for x in data["Time Series (15min)"]:
     # from each dictionary, get the low value "3. low"
-    lowlist.append(data["Time Series (5min)"][x]["3. low"])
+    lowlist.append(data["Time Series (15min)"][x]["3. low"])
 
 # also return the highest stock value <-- "2. high"
 # looping over all time dictionaries
 for y in data["Time Series (15min)"]:
-    high_list.append(data["Timeseries (15min)"][y]"2. high"])
-print(min(lowlist))
-print(max(high_list))
+    high_list.append(data["Time Series (15min)"][y]["2. high"])
+
+# save those values as a variable
+smallest= min(lowlist)
+highest= max(high_list)
+
+print(smallest)
+print(highest)
 
 # to show the percentae change of highest and lowest price 
-if lenght(lowlist) == length(high_list):
-    for i in len(lowlist): 
-        print(f'{(high_list[i]}  {low_list[i]}  {(((lowlist[i] - high_list[i]) / lowlist) * 100))}')  
+
+
+if len(lowlist) == len(high_list):
+    for i in range(0, len(lowlist)): 
+        #print(lowlist)
+        #print(high_list)
+        x= float( high_list[i]) - float(lowlist[i])
+        y= x / float(lowlist[i]) # tito needs to fix this cause chad is dumb :(
+        y= round(y * 100, 2)
+        print(f"{high_list[i]}  {lowlist[i]}  {y}%")  
+
+
+
+
