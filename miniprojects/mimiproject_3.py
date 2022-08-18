e7fe24725d0d971401691b3b38be5170d94fa15d
Tito Roy <roytito@yahoo.com>
2022-08-18 20:44:39 +0000

    test

--- miniprojects/mimiproject_3.py
+++ miniprojects/mimiproject_3.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+
+#to know the present status of network devices
+
 from netmiko import Netmiko
 from getpass import getpass
 
+#password object is created to login to the network devices
 password = getpass()
 
-device_list = [cisco1 = {
+#creating a device inventory list for to control 
+cisco1 = {
     'host': 'cisco1.twb-tech.com', 
     'username': 'pyclass', 
     'password': password,
@@ -30,8 +35,10 @@ srx1 = {
     'username': 'pyclass', 
     'password': password,
     'device_type': 'juniper_junos',
-}]
+}
 
-for device in (cisco1, cisco2, nxs1, sr1):
+#loop over the devices on the list
+for device in (cisco1, cisco2, nxos1, srx1):
     net_connect = Netmiko(**device)
-    print(net_connect.find_prompt())
+#display the last prompt of each devices
+print(net_connect.find_prompt())
